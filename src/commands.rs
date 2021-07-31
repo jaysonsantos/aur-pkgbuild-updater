2634c7c895223824883e5dbdbbd93282fc0bc917
Jayson Reis <santosdosreis@gmail.com>
2021-08-01 00:16:33 +0200

    feat: Breakdown commands to make packages run standalone

---
+++ src/commands.rs
@@ -0,0 +1,44 @@
+use color_eyre::{
+    eyre::{eyre, Context},
+    Result,
+};
+use tracing::{error, instrument};
+
+use crate::{package::Package, URL};
+
+#[instrument]
+pub async fn process_user(username: &str) -> Result<()> {
+    let mut should_exit_with_failure = false;
+
+    let mut packages = list_user_packages(username).await?;
+    for package in packages.iter_mut() {
+        if let Err(e) = package.process().await {
+            error!(
+                message = "Skipping package because of an error",
+                ?package,
+                error = ?e
+            );
+            should_exit_with_failure = true;
+        }
+    }
+
+    if should_exit_with_failure {
+        return Err(eyre!("Failed to process all packages"));
+    }
+
+    Ok(())
+}
+
+#[instrument]
+pub async fn list_user_packages(username: &str) -> Result<Vec<Package>> {
+    Package::parse_packages(&URL.replace("username", &username)).await
+}
+
+#[instrument]
+pub async fn process_package(package_name: &str) -> Result<()> {
+    let mut package = Package::new(package_name);
+    package
+        .process()
+        .await
+        .wrap_err("failed to process package")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+mod commands;
 mod package;
 mod version_checker;
 
-use std::{io, process::exit};
-
 use camino::{Utf8Path, Utf8PathBuf};
 use color_eyre::{eyre::WrapErr, Result};
+use commands::process_package;
 use directories::ProjectDirs;
 use lazy_static::lazy_static;
 use reqwest::Client;
-use structopt::StructOpt;
+use serde::{Deserialize, Serialize};
+use structopt::{clap::arg_enum, StructOpt};
 use tokio::fs;
-use tracing::error;
 use tracing_error::ErrorLayer;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
-use crate::package::Package;
+use crate::commands::{list_user_packages, process_user};
 
 const URL: &str = "https://aur.archlinux.org/packages/?K=username&SeB=m";
 
@@ -42,40 +42,52 @@ lazy_static! {
 }
 
 #[derive(Debug, StructOpt)]
-struct Arguments {
-    #[structopt(short, long)]
-    username: String,
+enum Arguments {
+    ProcessPackage {
+        #[structopt(short, long)]
+        package_name: String,
+    },
+    ProcessUser {
+        #[structopt(short, long)]
+        username: String,
+    },
+    ListUserPackages {
+        #[structopt(short, long)]
+        username: String,
+        #[structopt(short, long)]
+        output_type: OutputType,
+    },
+}
+
+arg_enum! {
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    enum OutputType {
+        Json,
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     setup_error_handlers()?;
     let args = Arguments::from_args();
-    let cache_dir = &*CACHE_DIR;
-    match fs::create_dir(cache_dir).await {
-        Ok(_) => {}
-        Err(e) if e.kind() == io::ErrorKind::AlreadyExists => {}
-        e => return e.wrap_err("failed to create cache_dir"),
-    }
 
-    write_helper_script().await?;
-    let mut should_exit_with_failure = false;
-
-    let mut packages = Package::parse_packages(&URL.replace("username", &args.username)).await?;
-    for package in packages.iter_mut() {
-        if let Err(e) = package.process().await {
-            error!(
-                message = "Skipping package because of an error",
-                ?package,
-                error = ?e
-            );
-            should_exit_with_failure = true;
+    match args {
+        Arguments::ProcessPackage { package_name } => process_package(&package_name).await?,
+        Arguments::ProcessUser { username } => process_user(&username).await?,
+        Arguments::ListUserPackages {
+            username,
+            output_type: _,
+        } => {
+            let packages = list_user_packages(&username)
+                .await
+                .wrap_err("failed to list user's packages")?;
+            let packages: Vec<&str> = packages.iter().map(|p| p.name.as_str()).collect();
+            let json_output = serde_json::to_string(&packages)?;
+            println!("{}", json_output);
         }
     }
 
-    if should_exit_with_failure {
-        exit(1);
-    }
+    write_helper_script().await?;
 
     Ok(())
 }
